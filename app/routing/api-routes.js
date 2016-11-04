ca1c5598c24367ffc8cedc65464f8c130608fc22
Maria Beane <mbeane@findyr.com>
2016-11-03 23:18:57 -0500

    routes added

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,5 +1,3 @@
-var path = require('path');
-
 var friendsData = require('../data/friends.js');
 
 module.exports = function(app) {
@@ -9,7 +7,7 @@ module.exports = function(app) {
 		res.json(friendsData);
 	});
 	//A POST routes /api/friends . This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
-	app.post('api/tables', function(req, res) {
+	app.post('/api/friends', function(req, res) {
 		friendsData.push(req.body);
 		res.json(true);
 	});
